b3a4827e286fc28dabfabcfb5947628e7233cfc2
honda <honshin.0411@gmail.com>
2024-02-07 20:36:45 +0900

    ……

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,6 +1,8 @@
 namespace Hoshiyama
 {
-	const char* TextMessage1 = "私の名前は三郎です。";
-	const char* TextMessage2 = "私の年齢は20歳です。";
-	const char* TextMessage3 = "私の好きな食べ物は焼肉です。";
+	const char* TextMessage1 = "　　／⌒ヽ";
+	const char* TextMessage2 = " く/･〝　⌒ヽ";
+	const char* TextMessage3 = "　 |　3　(∪￣]";
+	const char* TextMessage4 = "　く､･〟 (∩￣]";
+	const char* TextMessage5 = "￣￣￣￣￣￣￣￣";
 }
