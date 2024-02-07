0c4f3e5f455db8a23096f53983f2342dedd23045
Taiki <aikitaiki@icloud.com>
2024-02-07 20:25:37 +0900

    大丈夫君ならいける

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,6 +1,6 @@
-namespace Hoshiyama
+namespace Honda
 {
-	const char* TextMessage1 = "私の名前は無理太郎です。";
+	const char* TextMessage1 = "私の名前はいける太郎です。";
 	const char* TextMessage2 = "私の年齢は20歳です。";
 	const char* TextMessage3 = "私の好きな食べ物は焼肉です。";
 }
