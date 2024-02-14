3b97cbb49b9d5cf28f4e1dad8aca4e9b8f85c506
honda <honshin.0411@gmail.com>
2024-02-14 19:07:59 +0900

    なんかある？

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,19 +1,4 @@
 namespace Hoshiyama
 {
-	const char* TextMessage1  = "<(＾o＾)>　わたしのー							";
-	const char* TextMessage2  = "　　（　）										";
-	const char* TextMessage3  = "　／／											";
-	const char* TextMessage4  = "												";
-	const char* TextMessage5  = "<(＾o＾)>　　おはかのまえでー					";
-	const char* TextMessage6  = "（　）											";
-	const char* TextMessage7  = "　＼＼											";
-	const char* TextMessage8  = "												";
-	const char* TextMessage9  = "..三　　　　<(＾o＾)>　なかないでくださいー	";
-	const char* TextMessage10 = "　三　　　　（　）								";
-	const char* TextMessage11 = "三　　　　／／									";
-	const char* TextMessage12 = "												";
-	const char* TextMessage13 = "												";
-	const char* TextMessage14 = ". 　　　　<(＾o＾)>　　　三					";
-	const char* TextMessage15 = "　　　　　（　）　　　　三						";
-	const char* TextMessage16 = "　　　　　 ＼＼　　三							";
+	const char* TextMessage1  = "みんなpaizaやる感じ？";
 }
